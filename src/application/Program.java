5c6d16a5fc07bb128bb004b96e22d86aa690dc84
Gui Omena <gui.omenas@gmail.com>
2025-09-24 10:36:56 -0300

    Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
+
+        Position po = new Position(5,8);
+
+        System.out.println(po);
     }
 }
